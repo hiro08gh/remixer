8ddbf5494cf629bda0a9d3bb10dcdee68f74dee1
hiro08gh <hiro08gh@gmail.com>
2020-07-09 14:10:19 +0900

    update: target test file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/__tests__/**/(*.)+(spec|test)+(ts|tsx|js)',
     '**/?(*.)+(spec|test).+(ts|tsx|js)'
   ],
   preset: 'ts-jest',
